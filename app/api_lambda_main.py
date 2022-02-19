84ca5d810bda3bc1fcd085aeadf9fdd6a6859f22
Amjad Aldayem <aldaimjad1@gmail.com>
2022-02-18 16:48:00 -0800

    Fixed wrong import

--- app/api_lambda_main.py
+++ app/api_lambda_main.py
@@ -1,10 +1,7 @@
 # Entrypoint for API Lambda
 
 from mangum import Mangum
-
+from app import settings  # noqa
 from app.web.api import app
-from .main import initialize
-
-initialize()
 
 handler = Mangum(app=app)
