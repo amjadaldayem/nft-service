9126c61d32331560869260490ad72bd5cd4cbf1b
Amjad Aldayem <aldaimjad1@gmail.com>
2022-02-18 20:50:27 -0800

    Fixed JSON decode bug

--- app/web/api/dependencies.py
+++ app/web/api/dependencies.py
@@ -11,15 +11,15 @@ from ...utils import notify_error
 def get_auth_user(
         authorization: str = Header(None)
 ) -> User:
-    # try:
-    user_data = services.user_service.extract_token(
-        authorization,
-        orjson.loads(os.environ['COGNITO_PUBLIC_KEYS']),
-        int(os.environ.get('VERIFY_TOKEN', '0'))
-    )
-    # except Exception as e:
-    #     notify_error(e, metadata={'token': authorization})
-    #     raise AuthenticationError(data={'details': str(e)})
+    try:
+        user_data = services.user_service.extract_token(
+            authorization,
+            orjson.loads(os.getenv('COGNITO_PUBLIC_KEYS', "[]")),
+            int(os.environ.get('VERIFY_TOKEN', '0'))
+        )
+    except Exception as e:
+        notify_error(e, metadata={'token': authorization})
+        raise AuthenticationError(data={'details': str(e)})
     user = services.user_service.get_user(user_data['sub'])
     if not user:
         raise UserNotFound(data={'details': f"User sub = {user_data['sub']}"})
