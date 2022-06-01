1c6e9fcd4a969f31580094ef42acfffcce5a1da5
Amjad Aldayem <aldaimjad1@gmail.com>
2022-06-01 16:26:00 +0200

    Implement query builder for OpenSearch client

---
+++ sintra/api/open_search/query.py
@@ -0,0 +1,13 @@
+from typing import Any, Dict
+
+
+class QueryBuilder:
+    def read_token_query(self, token_id: str) -> Dict[str, Any]:
+        return {"query": {"match": {"token_id.keyword": token_id}}}
+
+    def read_tokens_query(self) -> Dict[str, Any]:
+        return {
+            "size": 20,
+            "sort": {"timestamp_of_market_activity": {"order": "desc"}},
+            "query": {"match_all": {}},
+        }
