e0355ffc0126f5459a853907cf6a1a04f6c0b2d5
Amjad Aldayem <aldaimjad1@gmail.com>
2022-06-21 16:22:35 +0200

    Implement Magic Eden signature parser tests

--- sintra/lambda/signature-instruction/tests/parser/test_magic_eden.py
+++ sintra/lambda/signature-instruction/tests/parser/test_magic_eden.py
@@ -15,12 +15,58 @@ from src.parser.magic_eden import (
 
 class TestMagicEdenParser:
     @pytest.mark.parametrize(
-        "event_name, event_value", [("sale", 3), ("listing", 1), ("delisting", 2)]
+        "event_name, event_value, blockchain_id, market_id, blocktime, token_key, price, owner, buyer, transaction_hash",
+        [
+            (
+                "listing",
+                1,
+                65536,
+                65793,
+                1635223673,
+                "rp9gXpW4zAetpWBvJY62uTSw1bRwqNd39b2f3hzaNwk",
+                4000000000,
+                "CA6WUwiH8E9Z6ZYJvjNkKAV6QPq7ySWvLTT8fW4CNPw4",
+                None,
+                "415NNVREVfHMYciwuCfMX1QYZYRgUQVyMobBbYbeU8GCfJUEt7x6sXiM96igMyGsRb3dxzCMbcvvPufPWrNwZsN5",
+            ),
+            (
+                "delisting",
+                2,
+                65536,
+                65793,
+                1637214793,
+                "rp9gXpW4zAetpWBvJY62uTSw1bRwqNd39b2f3hzaNwk",
+                0,
+                "CA6WUwiH8E9Z6ZYJvjNkKAV6QPq7ySWvLTT8fW4CNPw4",
+                None,
+                "3B9ZCpuMsHm5q1f8JKw8psFq5yq8CNcQytmc47km3qYmLXwHtvqibMGYEnHG9GTnmHyZa3caXxWFD1pFs2hSy2LB",
+            ),
+            (
+                "sale",
+                3,
+                65536,
+                65793,
+                1636475946,
+                "8ag4raw8snR6GbM6znQYv9k4845zs5NYrDZdoxnxaNwK",
+                10000000000,
+                None,
+                "BXxoKHT6CYtRTboZDaJee1ahrHoQSN6RJk3HAKpUWGHa",
+                "5aQpquskPCgjMQU5Rp1EktgBe4mcRtMVnTMKQKjvPFv3PVyW51DhzLWR6nFz6Ptrg3yEB2dg1HvVm7d7tqAapNVo",
+            ),
+        ],
     )
     def test_magic_eden_v1_parser(
         self,
         event_name,
         event_value,
+        blockchain_id,
+        market_id,
+        blocktime,
+        token_key,
+        price,
+        owner,
+        buyer,
+        transaction_hash,
         magic_eden_v1_transaction_path: Path,
         magic_eden_v1_parser: MagicEdenParserV1,
     ) -> None:
@@ -34,6 +80,15 @@ class TestMagicEdenParser:
             secondary_market_event = magic_eden_v1_parser.parse(transaction)
 
             assert secondary_market_event.event_type == event_value
+            assert secondary_market_event.blockchain_id == blockchain_id
+            assert secondary_market_event.market_id == market_id
+            assert secondary_market_event.blocktime == blocktime
+            assert secondary_market_event.token_key == token_key
+            assert secondary_market_event.price == price
+            assert secondary_market_event.owner == owner
+            assert secondary_market_event.buyer == buyer
+            assert secondary_market_event.transaction_hash == transaction_hash
+
             assert isinstance(secondary_market_event, SecondaryMarketEvent)
 
     def test_magic_eden_v1_parser_bid_event(
@@ -50,13 +105,70 @@ class TestMagicEdenParser:
                 magic_eden_v1_parser.parse(transaction)
 
     @pytest.mark.parametrize(
-        "event_name, event_value",
-        [("bid", 5), ("sale", 3), ("listing", 1), ("delisting", 2)],
+        "event_name, event_value, blockchain_id, market_id, blocktime, token_key, price, owner, buyer, transaction_hash",
+        [
+            (
+                "listing",
+                1,
+                65536,
+                65793,
+                1645815709,
+                "As1cySAyfeesM4MBrYZhs46DFMfnHH3ySU32C2xfSgPv",
+                8000000000,
+                "CA6WUwiH8E9Z6ZYJvjNkKAV6QPq7ySWvLTT8fW4CNPw4",
+                "",
+                "3hWD6HQxUhWLChkHQBCisQHb88qGK2cAWkXh7ixHpe1QfYvuMgr4JHAH6aN8767cCBaNzkjDQv1x2CTTApX5nXsn",
+            ),
+            (
+                "delisting",
+                2,
+                65536,
+                65793,
+                1645816064,
+                "As1cySAyfeesM4MBrYZhs46DFMfnHH3ySU32C2xfSgPv",
+                0,
+                "CA6WUwiH8E9Z6ZYJvjNkKAV6QPq7ySWvLTT8fW4CNPw4",
+                "",
+                "5y2MJLobJ1Z5fXdQN6ojg8b2o9yMV3nMpqn5XV1ArLbq6Fe7iAyDACJn26uAYmx8Cj6Qc588Ku1jvREzTYQrux9s",
+            ),
+            (
+                "sale",
+                3,
+                65536,
+                65793,
+                1645817729,
+                "CqkUbXgnYhwxfzJRqPVJGparRRsrzMJKGGUuL59Gsajj",
+                30000000,
+                "",
+                "2ivnxDtJ3KbyYF2EivgMNFfKZrUNMviumPqnvL9T77aZ",
+                "2F94RKPUcekFsmK8D4BCcK2y3nadcEerfX3kcPBujBTVNpNrUtPFGSN7uA8FGbxzpeAKWLLaqRNY5BCbg5zY2k8t",
+            ),
+            (
+                "bid",
+                5,
+                65536,
+                65793,
+                1645817399,
+                "CqkUbXgnYhwxfzJRqPVJGparRRsrzMJKGGUuL59Gsajj",
+                30000000,
+                "",
+                "2ivnxDtJ3KbyYF2EivgMNFfKZrUNMviumPqnvL9T77aZ",
+                "3gzq2Txq7u9W2LJpENaV1BJN1m4NnYxKAUC9Zhs1xz9wdt8XgQa46PxGpEmztyRd18v2YV59FEdRVfHcHysjNiKA",
+            ),
+        ],
     )
     def test_magic_eden_v2_parser(
         self,
         event_name,
         event_value,
+        blockchain_id,
+        market_id,
+        blocktime,
+        token_key,
+        price,
+        owner,
+        buyer,
+        transaction_hash,
         magic_eden_v2_transaction_path: Path,
         magic_eden_v2_parser: MagicEdenParserV2,
     ) -> None:
@@ -70,15 +182,51 @@ class TestMagicEdenParser:
             secondary_market_event = magic_eden_v2_parser.parse(transaction)
 
             assert secondary_market_event.event_type == event_value
+            assert secondary_market_event.blockchain_id == blockchain_id
+            assert secondary_market_event.market_id == market_id
+            assert secondary_market_event.blocktime == blocktime
+            assert secondary_market_event.token_key == token_key
+            assert secondary_market_event.price == price
+            assert secondary_market_event.owner == owner
+            assert secondary_market_event.buyer == buyer
+            assert secondary_market_event.transaction_hash == transaction_hash
+
             assert isinstance(secondary_market_event, SecondaryMarketEvent)
 
+    @pytest.mark.parametrize(
+        "event_name, event_value, blockchain_id, market_id, blocktime, token_key, price, owner, buyer, transaction_hash",
+        [
+            (
+                "price_update",
+                1,
+                65536,
+                65793,
+                1645817644,
+                "CqkUbXgnYhwxfzJRqPVJGparRRsrzMJKGGUuL59Gsajj",
+                50000000,
+                "CA6WUwiH8E9Z6ZYJvjNkKAV6QPq7ySWvLTT8fW4CNPw4",
+                "",
+                "3He3nAQ4Xg4veRWUDysPC71YPGvy5QMc8frakkvYL5tQ25VUpmFDuMt7rx5tjGLKvNp8tPhs1ot61hvt8TvYuxqw",
+            ),
+        ],
+    )
     def test_magic_eden_v2_parser_price_update_is_listing(
         self,
+        event_name,
+        event_value,
+        blockchain_id,
+        market_id,
+        blocktime,
+        token_key,
+        price,
+        owner,
+        buyer,
+        transaction_hash,
         magic_eden_v2_transaction_path: Path,
         magic_eden_v2_parser: MagicEdenParserV2,
     ) -> None:
         with open(
-            magic_eden_v2_transaction_path / "price_update.json", "r"
+            magic_eden_v2_transaction_path / (event_name + ".json"), "r"
         ) as json_file:
             json_event = json_file.read()
             transaction_dict = json.loads(json_event)
@@ -86,7 +234,16 @@ class TestMagicEdenParser:
 
             secondary_market_event = magic_eden_v2_parser.parse(transaction)
 
-            assert secondary_market_event.event_type == 1
+            assert secondary_market_event.event_type == event_value
+            assert secondary_market_event.blockchain_id == blockchain_id
+            assert secondary_market_event.market_id == market_id
+            assert secondary_market_event.blocktime == blocktime
+            assert secondary_market_event.token_key == token_key
+            assert secondary_market_event.price == price
+            assert secondary_market_event.owner == owner
+            assert secondary_market_event.buyer == buyer
+            assert secondary_market_event.transaction_hash == transaction_hash
+
             assert isinstance(secondary_market_event, SecondaryMarketEvent)
 
     @pytest.mark.parametrize(
